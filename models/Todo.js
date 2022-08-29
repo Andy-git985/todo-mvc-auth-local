85dff7f62103421100a39b3cf33bcf9055460fb7
Andy-git985 <andy.yu617@gmail.com>
2022-08-28 21:33:40 -0400

    todoschema

--- models/Todo.js
+++ models/Todo.js
@@ -1,33 +1,13 @@
-// const mongoose = require('mongoose');
-
-// const TodoSchema = new mongoose.Schema({
-//   todo: {
-//     type: String,
-//     required: true,
-//   },
-//   completed: {
-//     type: Boolean,
-//     required: true,
-//   },
-//   userId: {
-//     type: String,
-//     required: true,
-//   },
-// });
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const TodoSchema = new Schema(
-  {
-    title: String,
-    description: String,
-    userId: {
-      type: String,
-      required: true,
-    },
+const TodoSchema = new Schema({
+  title: String,
+  description: String,
+  userId: {
+    type: String,
+    required: true,
   },
-  { collection: 'todo-list' }
-);
+});
 
 module.exports = mongoose.model('Todo', TodoSchema);
