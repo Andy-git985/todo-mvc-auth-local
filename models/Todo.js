a4d5fcf1afc9464a6333ffd1fc68a80abf653415
Andy-git985 <andy.yu617@gmail.com>
2022-08-28 21:21:44 -0400

    testing todos

--- models/Todo.js
+++ models/Todo.js
@@ -1,18 +1,33 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose');
 
-const TodoSchema = new mongoose.Schema({
-  todo: {
-    type: String,
-    required: true,
-  },
-  completed: {
-    type: Boolean,
-    required: true,
-  },
-  userId: {
-    type: String,
-    required: true
-  }
-})
+// const TodoSchema = new mongoose.Schema({
+//   todo: {
+//     type: String,
+//     required: true,
+//   },
+//   completed: {
+//     type: Boolean,
+//     required: true,
+//   },
+//   userId: {
+//     type: String,
+//     required: true,
+//   },
+// });
 
-module.exports = mongoose.model('Todo', TodoSchema)
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  { collection: 'todo-list' }
+);
+
+module.exports = mongoose.model('Todo', TodoSchema);
